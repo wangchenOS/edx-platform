199b5e98725d282632dbf1ffd99b2d00a4dfdc66
Afzal Wali <afzal.wali@arbisoft.com>
2014-12-19 16:15:34 +0500

    Added the reportlab requirement to base.txt
    Added receipts_pdf.py
    
    Used Paragraph for displaying a large body of text.
    
    added the table
    
    Line breaks in the para text. font size adjusted.
    
    Improved the main table (alignments) and totals (converted to a table as well)
    
    Converted the footer into a table, and allowed for pagination.
    
    Added pagination to item data table.
    
    Handled wrapping of long descriptions into multiple lines.
    
    email attachment for both invoice and receipt
    
    added the currency from the settings
    
    Removed magic numeric literals and added meaningful variables.
    
    Added initial set of substitutions from configuration
    
    add defining logo paths via configuration
    
    Removed font dependencies. Will use the system default fonts which appear good enough to me.
    
    Alignment adjustments as per suggestions.
    
    Fixed the pep8 violations. Added comments to styling
    
    added the decimal points to the price values
    
    Cleanup. Docstrings.
    
    i18n the text in the pdf file
    
    fix pep8/pylint issues
    
    Changed the amounts from string to float.
    
    Overrode the 'pdf_receipt_display_name' property in the OrderItem subclass Donation.
    
    used the PaidCourseRegistration instead of the parent OrderItem to avoid course_id related exceptions.
    
    quality fixes
    
    added  the test cases for the pdf
    
    made the changes in the pdf suggested by griff

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -1943,3 +1943,17 @@ COURSE_CATALOG_VISIBILITY_PERMISSION = 'see_exists'
 # which access.py permission name to check in order to determine if a course about page is
 # visible. We default this to the legacy permission 'see_exists'.
 COURSE_ABOUT_VISIBILITY_PERMISSION = 'see_exists'
+
+# Configuration used for generating PDF Receipts/Invoices
+PDF_RECEIPT_TAX_ID = 'add here'
+PDF_RECEIPT_FOOTER_TEXT = 'add your own specific footer text here'
+PDF_RECEIPT_DISCLAIMER_TEXT = 'add your own specific disclaimer text here'
+PDF_RECEIPT_BILLING_ADDRESS = 'add your own billing address here with appropriate line feed characters'
+PDF_RECEIPT_TERMS_AND_CONDITIONS = 'add your own terms and conditions'
+PDF_RECEIPT_TAX_ID_LABEL = 'Tax ID'
+PDF_RECEIPT_LOGO_PATH = PROJECT_ROOT + '/static/images/openedx-logo-tag.png'
+# Height of the Logo in mm
+PDF_RECEIPT_LOGO_HEIGHT_MM = 12
+PDF_RECEIPT_COBRAND_LOGO_PATH = PROJECT_ROOT + '/static/images/logo-placeholder.png'
+# Height of the Co-brand Logo in mm
+PDF_RECEIPT_COBRAND_LOGO_HEIGHT_MM = 12
